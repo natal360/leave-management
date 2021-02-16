05011eff7ce5128674899955b79aa1cb2fd62d78
natal <drossel.fb@gmail.com>
2021-02-16 16:42:07 +0900

    プロジェクト ファイルを追加します。

---
+++ leave-management/Models/Mappings/Maps.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+        }
+    }
+}
