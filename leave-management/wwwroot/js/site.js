174217de081d72d83c7dc37c6f4f2c8613dc8de8
natal <drossel.fb@gmail.com>
2021-02-18 13:03:07 +0900

    Setup User Auth, Default roles and admin, modified registration process

--- leave-management/wwwroot/js/site.js
+++ leave-management/wwwroot/js/site.js
@@ -2,6 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//$(document).ready(function () {
+//    $('#tblData').DataTable({
+//        fixedHeader: true,
+//        "language": {
+//           url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Japanese.json"
+//        }
+//    });
+//});
+
 $(document).ready(function () {
-    $('#tblData').DataTable();
+    var table = $('#tblData').DataTable({
+        fixedHeader: true,
+        "language": {
+           url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Japanese.json"
+        }
+    });
 });
